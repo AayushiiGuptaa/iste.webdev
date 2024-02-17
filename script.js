93c9c2fe6e944e3419c2bc63607ae348a62c77bb
AayushiiGuptaa <149000126+AayushiiGuptaa@users.noreply.github.com>
2024-02-17 23:45:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const artworks = document.querySelectorAll('.artwork');
-
-  artworks.forEach((artwork, index) => {
-    artwork.addEventListener('click', () => {
-      alert(`You clicked on Artwork ${index + 1}`);
-    });
-  });
-});
